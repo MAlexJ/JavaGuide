10f232eec9772c67eb7f535d58da5a8ec5d57a46
MAlexj <alexmaximovj@gmail.com>
2016-03-19 21:01:06 +0200

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,22 @@
 /**
  * Created by malex on 17.03.16.
  */
+
+// continue, break,
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(1111);
+        System.out.print("Begin ");
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) {
+                //1
+                  //2
+                   // 3
+                throw new RuntimeException();
+            }
+            System.out.print(i+" ");
+        }
+        System.out.println("End");
     }
 }
