67da7738ca741c8e722ee6e246df47af25100974
malexj <alexmaximovj@gmail.com>
2016-03-26 01:13:44 +0200

    fix

--- src/main/java/chapter_08_Thread/example_05_synchronized/Example_10.java
+++ src/main/java/chapter_08_Thread/example_05_synchronized/Example_10.java
@@ -1,7 +1,47 @@
 package chapter_08_Thread.example_05_synchronized;
 
+import static chapter_08_Thread.example_05_synchronized.Example_10.num;
+
 public class Example_10 {
+    static int num;
+
     public static void main(String[] args) {
+        Common common = new Common();
+
+        for (int i = 1; i < 4; i++){
+            Thread t = new Thread(new MyThreadRun(common));
+            t.setName("Поток "+ i);
+            t.start();
+        }
+    }
+}
+
+class Common {
+
+}
+
+class MyThreadRun implements Runnable {
+
+    private final Common common;
+
+    MyThreadRun(Common common) {
+        this.common = common;
+    }
+
+    @Override
+    public void run() {
+        synchronized (common) {
+            num = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.printf("%s %d \n", Thread.currentThread().getName(), num);
+                num++;
+                try {
+                    Thread.sleep(120);
+                } catch (Exception ignored) {
+                    //Ignore
+                }
+            }
+        }
 
     }
 }
